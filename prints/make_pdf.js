dccc26ef25162838c4a3d7d9002865c465f22186
Sam Lavigne <splavigne@gmail.com>
2023-07-27 14:51:09 -0400

    ok

--- prints/make_pdf.js
+++ prints/make_pdf.js
@@ -2,27 +2,23 @@ const fs = require("fs");
 const path = require("path");
 const puppeteer = require("puppeteer");
 
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 async function render(page, filename, _options = {}) {
-  const defaults = {
-    width: 18,
-    height: 24,
-    margin: 0.5,
-    lineHeight: 1,
-    columns: 3,
-    minFontSize: 11,
-  };
+	const defaults = {
+		width: 18,
+		height: 24,
+		margin: 0.5,
+		lineHeight: 1,
+		columns: 3,
+		minFontSize: 11,
+	};
 
-  const options = { ...defaults, ..._options };
+	const options = { ...defaults, ..._options };
 
-  const htmlFile = path.resolve(filename);
+	const htmlFile = path.resolve(filename);
 
-  const content = fs.readFileSync(htmlFile, "utf8");
+	const content = fs.readFileSync(htmlFile, "utf8");
 
-  const css = `<style>
+	const css = `<style>
     body {
       box-sizing: border-box;
       line-height: ${options.lineHeight};
@@ -49,7 +45,7 @@ async function render(page, filename, _options = {}) {
     }
   </style>`;
 
-  const html = `
+	const html = `
     <!DOCTYPE html>
     <html lang="en">
     <head>
@@ -64,64 +60,72 @@ async function render(page, filename, _options = {}) {
     </html>
   `;
 
-  await page.goto(`data:text/html,${html}`, { waitUntil: "networkidle0" });
+	await page.goto(`data:text/html,${html}`, { waitUntil: "networkidle0", timeout: 0 });
 
-  await page.evaluate((options) => {
-    function isOverflown(element) {
-      return (
-        element.scrollHeight > element.clientHeight ||
-        element.scrollWidth > element.clientWidth
-      );
-    }
+	await page.evaluate((options) => {
+		function isOverflown(element) {
+			return (
+				element.scrollHeight > element.clientHeight ||
+				element.scrollWidth > element.clientWidth
+			);
+		}
 
-    function setPage(pages=1) {
-      document.body.style.height = `${options.height * pages + options.margin*(pages)*2}in`;
+		function setPage(pages = 1) {
+			document.body.style.height = `${options.height * pages + options.margin * (pages) * 2}in`;
 
-      let fontSize = options.minFontSize * 2;
-      document.body.style.fontSize = fontSize + "pt";
+			let fontSize = options.minFontSize * 2;
+			document.body.style.fontSize = fontSize + "pt";
 
-      while (isOverflown(document.body)) {
-        fontSize -= 0.1;
-        document.body.style.fontSize = fontSize + "pt";
-      }
+			while (isOverflown(document.body)) {
+				fontSize -= 0.1;
+				document.body.style.fontSize = fontSize + "pt";
+			}
 
-      if (fontSize < options.minFontSize) {
-        setPage(pages + 1);
-      } else {
-        // document.body.style.height = "auto";
-        document.body.style.padding = "0";
-      }
-    }
+			if (fontSize < options.minFontSize) {
+				setPage(pages + 1);
+			} else {
+				// document.body.style.height = "auto";
+				document.body.style.padding = "0";
+			}
+		}
 
-    setPage(1);
+		setPage(1);
 
-  }, options);
+	}, options);
 
-  const outname = `${path.basename(filename)}.${options.width}_${
-    options.height
-  }.pdf`;
+	const outname = `renders/${path.basename(filename)}_${options.width}x${options.height}_${options.margin}margin_${options.columns}cols_${options.minFontSize}font.pdf`;
 
-  await page.pdf({
-    path: outname,
-    preferCSSPageSize: true,
-  });
+	await page.pdf({
+		path: outname,
+		preferCSSPageSize: true,
+	});
 }
 
 async function main() {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+	const browser = await puppeteer.launch({ headless: true });
+	const page = await browser.newPage();
 
-  const options = {
-    width: 20,
-    height: 28,
-    margin: 0.5,
-    lineHeight: 1,
-    columns: 3,
-    minFontSize: 10,
-  };
-  await render(page, "../docs/sorts/all my.html", options);
+	const options = {
+		width: 20,
+		height: 128,
+		margin: 0.5,
+		lineHeight: 1,
+		columns: 2,
+		minFontSize: 6,
+	};
+	await render(page, "../docs/sorts/all my.html", options);
 
-  await browser.close();
+	// const options = {
+	// 	width: 24,
+	// 	height: 128,
+	// 	margin: 0.5,
+	// 	lineHeight: 1,
+	// 	columns: 3,
+	// 	minFontSize: 6,
+	// };
+	// await render(page, "everything.html", options);
+
+	await browser.close();
 }
 
 main();
